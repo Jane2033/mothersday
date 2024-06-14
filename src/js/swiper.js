96bfeb1813d463699ebe1a34a409b2481300d072
Anya <zhangyazhen@zhangyahendeMBP.home>
2024-06-14 11:14:32 +0800

    initial commit

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,11 +1,10 @@
 const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    rewind: true,
-    grabCursor: 'true',
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  slidesPerView: 1,
+  spaceBetween: 0,
+  rewind: true,
+  grabCursor: 'true',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
