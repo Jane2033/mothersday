7d38d99fccd6b71214f90a5459611d176f4b2622
Anya <zhangyazhen@zhangyahendeMBP.home>
2024-09-23 10:43:11 +0800

    元件修改

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,9 +2,9 @@ const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 0,
   rewind: true,
+  autoplay: 'true',
   grabCursor: 'true',
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  pagination: {
+    el: ".swiper-pagination",
   },
 });
